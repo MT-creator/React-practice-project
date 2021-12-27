1a834c8c073ba4ddec8003429835896599cccb8d
Creator <grgmilan1999@gmail.com>
2021-12-27 14:57:44 +0545

    Add files via upload

---
+++ WeatherApp/index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require('requests');
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+    return temperature;
+};
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        requests(
+            "https://api.openweathermap.org/data/2.5/weather?q=Kathmandu&appid=992d2e396b609644236b3f9a63050b12&units=metric"
+        )
+            .on("data", (chunk) => {
+                const objData = JSON.parse(chunk);
+                const arrObj = [objData]
+                //console.log(arrObj[0].main.temp);
+                const realTimeData = arrObj.map((val) => replaceVal(homeFile, val))
+                    .join("");
+                res.write(realTimeData);
+            })
+            .on("end", (err) => {
+                if (err) return console.log("Connection closed due to errors.", err);
+                console.log("end");
+            });
+    }
+    else {
+        res.end("File not found");
+    }
+
+});
+
+server.listen(8000, "127.0.0.1", () => {
+
+    console.log("running server ... ")
+});
